e144fc26f562550e241958ded6f509c9da25e4dd
mmdcloud <madmaxcloudpractice@gmail.com>
2025-09-18 10:14:53 +0530

    update: rename network security group to 'peering-nsg'

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "acceptanceTestSecurityGroup1"
+  name                = "peering-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
